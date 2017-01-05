3966b11b2724eb05ed4b6e91c83854e3fbdfd05f
LobinhaXD <milla@silverwolf.com.br>
2017-01-04 22:38:03 -0200

    update test mocha

--- test/app_test.js
+++ test/app_test.js
@@ -2,18 +2,18 @@ var app = require('../server'),
   assert = require('assert'),
   http = require('http');
 
+  
 describe('GET /', function(){
   before(function() {
-      var port = '8080';
+      var portapp = process.env.PORT || 8080;
       app.set('port', port);
       var server = http.createServer(app);
-      server.listen(app.get('port'), function(){            
-		console.log('Express server listening on port ' + app.get('port')); 
-	});
+      server.listen(app.get('port'));
   });
 
   it('should return a 200 status code', function (done){
-    http.get({ host: 'localhost', port: app.get('port') }, function(res) {
+	var portapp = process.env.PORT || 8080;
+    http.get({ host: '127.0.0.1', port: 'portapp' }, function(res) {
       assert.deepEqual(res.statusCode, 200);
       done();
     }).on('error', function(e) {
